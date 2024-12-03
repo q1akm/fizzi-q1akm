4584200615e12a678606db37af0fde07f48377a0
Aung Kaung Myint <aungkggmyint@gmail.com>
2024-12-03 10:04:47 +0700

    finish fizzi website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,19 @@
-import localFont from 'next/font/local'
+import localFont from "next/font/local";
+
 import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
 
-import './app.css'
+import "./app.css";
+import Header from "@/components/Header";
+import ViewCanvas from "@/components/ViewCanvas";
+import Footer from "@/components/Footer";
 
 const alpino = localFont({
-  src: '../../public/fonts/Alpino-Variable.woff2',
-  display: 'swap',
+  src: "../../public/fonts/Alpino-Variable.woff2",
+  display: "swap",
   weight: "100 900",
-  variable: "--font-alpino"
-})
+  variable: "--font-alpino",
+});
 
 export default function RootLayout({
   children,
@@ -18,7 +22,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={alpino.variable}>
-      <body>{children}</body>
+      <body className="overflow-x-hidden bg-yellow-300">
+        <Header />
+        <main>
+          {children}
+          <ViewCanvas />
+        </main>
+        <Footer />
+      </body>
       <PrismicPreview repositoryName={repositoryName} />
     </html>
   );
